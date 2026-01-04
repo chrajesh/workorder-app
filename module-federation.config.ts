2e61ff1afd89c997e7d09f61e57c3f66d1829ffc
rajesh <rchaganti@evoketechnologies.com>
2026-01-04 15:28:05 -0500

    updated url

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -2,7 +2,7 @@ export const mfConfig = {
   name: "workorder_app",
   exposes: {},
   remotes: {
-    home_app: "home_app@https://storage.cloud.google.com/home-app/mf-manifest.json",
+    home_app: "home_app@https://storage.googleapis.com/home-app-v2/mf-manifest.json",
   },
   shared: ["react", "react-dom"],
 };
