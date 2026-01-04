91e90f4e29e3143e4c382c1c93bd64298d4706c5
rajesh <rchaganti@evoketechnologies.com>
2026-01-04 14:48:32 -0500

    updated s3 location for home-app

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -2,7 +2,7 @@ export const mfConfig = {
   name: "workorder_app",
   exposes: {},
   remotes: {
-    home_app: "home_app@https://home-app-488099994870.us-south1.run.app/mf-manifest.json",
+    home_app: "home_app@https://storage.googleapis.com/home-app/mf-manifest.json",
   },
   shared: ["react", "react-dom"],
 };
